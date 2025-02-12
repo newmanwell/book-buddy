becbbf851eb9d5223fa8e342b38221cd449454d6
Shaun Newman <shaun.newman.p@gmail.com>
2025-02-12 12:09:04 -0500

    able to grab individual book objects

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const BookDetails = () => {
+  const { id } = useParams();
+  const [oneBook, setOneBook] = useState({});
+
+  useEffect(() => {
+    const getBookDetail = async() => {
+      try {
+        const respsonse = await fetch(`https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books/${id}`);
+        const bookObj = await respsonse.json();
+        const bookDetail = bookObj.book;
+        console.log(bookDetail);
+      } catch(error) {
+        console.log(error);
+      }
+    }
+    getBookDetail();
+  }, []);
+
+
+
   return (
     <h2>The Details</h2>
   )
