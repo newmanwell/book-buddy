4c9433050dc552afe420a3887470d8da474dbda3
Shaun Newman <shaun.newman.p@gmail.com>
2025-02-13 13:05:09 -0500

    checkout out button functions as expected

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -4,6 +4,8 @@ import { useParams } from "react-router-dom";
 const BookDetails = () => {
   const { id } = useParams();
   const [oneBook, setOneBook] = useState({});
+  // grab the token from local storage
+  const getToken = localStorage.getItem("token");
 
   useEffect(() => {
     const getBookDetail = async() => {
@@ -27,6 +29,25 @@ const BookDetails = () => {
     }
   }
 
+  const checkOutBook = async() => {
+    try {
+      const response = await fetch(`https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${getToken}`
+        },
+        body: JSON.stringify({
+        available: false
+        })
+      });
+      const result = await response.json();
+      console.log(result);
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
   return (
     <>
     
@@ -37,8 +58,9 @@ const BookDetails = () => {
               <h2>Title: { oneBook.title }</h2>
               <h2>Author: { oneBook.author }</h2>
               <h3>Is Available: { isAvailable() }</h3>
-              <img src={ oneBook.coverimage} alt="Book Cover" />
+              <img src={ oneBook.coverimage } alt="Book Cover" />
               <p>Description: { oneBook.description }</p>
+              <button onClick={ checkOutBook }>Check Out</button>
             </section>
           </div>
         :
